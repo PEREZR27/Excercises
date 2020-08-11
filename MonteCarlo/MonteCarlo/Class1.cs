fba92a68119e5bb37c1be866b967a81c2b401992
Richard Perez <PEREZR27@my.erau.edu>
2020-08-11 16:59:21 -0500

    Adds Class1.cs

---
+++ MonteCarlo/MonteCarlo/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonteCarlo
+{
+    public struct XY
+
+    {
+        public double x;
+        public double y;
+
+        public XY(double x, double y)
+
+        {
+            this.x = x;
+
+            this.y = y;
+        }
+
+        public XY(Random obj)
+
+        {
+            x = obj.NextDouble();
+
+            y = obj.NextDouble();
+        }
+
+        public double X()
+
+        {
+
+            return this.x;
+
+        }
+
+
+
+        public double Y()
+
+        {
+
+            return this.y;
+
+        }
+
+        public double Hypotenuse(double x, double y)
+
+        {
+
+            double hypotenuse = Math.Sqrt((Math.Pow(x, 2)) + (Math.Pow(y, 2)));
+
+            return hypotenuse;
+
+        }
+
+    }
+
+}
+
+
---
+++ MonteCarlo/MonteCarlo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input array size");
+
+            int input = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            XY hyp = new XY();
+
+
+            double[,] arr = new double[input, 2];
+
+            int length1 = arr.Length - input;
+
+            int counter = 0;
+
+
+            for (int i = 0; i < length1; ++i)
+
+            {
+
+                XY rand = new XY(rnd);
+
+                arr[i, 0] = rand.X();
+
+                arr[i, 1] = rand.Y();
+
+                Console.WriteLine($"X: {rand.X()}.  Y: {rand.Y()}");
+
+                Console.WriteLine($"Hyp: {hyp.Hypotenuse(rand.X(), rand.Y())}");
+
+
+                if (hyp.Hypotenuse(rand.X(), rand.Y()) <= 1)
+
+                {
+
+                    counter++;
+
+                }
+
+            }
+
+            double length = length1;
+            double coordinates = 0;
+            double total = counter;
+
+            coordinates += total / length;
+
+            double coordinatesTotal = coordinates * 4;
+
+
+            Console.WriteLine($"Counter: {counter}");
+
+            Console.WriteLine($"Result: {coordinatesTotal}");
+
+            double difference = Math.Abs(coordinatesTotal - Math.PI);
+
+            Console.WriteLine($"Difference: {difference}");
+
+
+            Console.WriteLine($" ");
+
+
+        }
+    }
+}
