304dee598dd1b489e607333cb1d437103e5f9d4e
Richard Perez <PEREZR27@my.erau.edu>
2020-08-11 17:15:58 -0500

    Adds ManipulatingArrays/App

---
+++ ManipulatingArrays/App.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManipulatingArrays
+{
+   public class App
+    {
+        public static int getLength(int[] arr)
+
+        {
+
+            int count = 0;
+
+            foreach (int number in arr) count++;
+
+            return count;
+
+        }
+        public static int getSum(int[] arr)
+
+        {
+
+            int sum = 0;
+
+            for (int i = 0; i < getLength(arr); i++) sum += arr[i];
+
+            return sum;
+
+        }
+
+        public static double getMean(int[] arr) => getSum(arr) / getLength(arr);
+
+        public static void reverse(int[] arr)
+
+        {
+
+            int[] reversed = new int[getLength(arr)];
+
+            int index = 0;
+
+            for (int i = getLength(arr) - 1; i >= 0; i--)
+
+            {
+
+                reversed[index] = arr[i];
+
+                index++;
+
+            }
+
+            displayArray(reversed);
+        }
+
+        public static void rotate(string orientation, int x, int[] arr)
+
+        {
+
+            int[] rotated = new int[getLength(arr)];
+
+            int index;
+
+            while (x > arr.Length)
+
+            {
+
+                x -= arr.Length;
+
+            }
+
+            if (orientation == "right")
+
+            {
+
+                index = 0 + x;
+
+                for (int i = 0; i < getLength(arr); i++)
+
+                {
+
+                    if (index == getLength(arr))
+
+                    {
+
+                        index = 0;
+
+                    }
+
+                    rotated[index] = arr[i];
+
+                    index++;
+
+                }
+
+            }
+
+            if (orientation == "left")
+
+            {
+
+                index = 0;
+
+                for (int i = x; index < getLength(arr); i++)
+
+                {
+
+                    if (i == getLength(arr))
+
+                    {
+
+                        i = 0;
+
+                    }
+
+                    rotated[index] = arr[i];
+
+                    index++;
+
+                }
+
+            }
+
+
+
+            displayArray(rotated);
+
+        }
+
+        public static void bubbleSort(int[] arr)
+
+        {
+
+            int temp = 0;
+
+
+
+            for (int i = 0; i < getLength(arr) - 1; i++)   
+
+            {
+
+                for (int j = i + 1; j < getLength(arr); j++)  
+
+                {
+
+                    if (arr[i] > arr[j])
+
+                    {
+
+                        temp = arr[i];     
+
+                        arr[i] = arr[j];   
+
+                        arr[j] = temp;
+
+                    }
+
+                }
+
+            }
+
+
+
+            displayArray(arr);
+
+        }
+
+
+        public static void displayArray(int[] arr)
+
+        {
+
+            foreach (int x in arr)
+
+            {
+
+                Console.Write(x + " ");
+
+            }
+
+            Console.WriteLine("");
+
+        }
+
+    }
+}
---
+++ ManipulatingArrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ManipulatingArrays
+{
+       public class Program
+
+        {
+            static void Main(string[] args)
+
+            {
+
+                   int[] A = new int[] { 0, 2, 4, 6, 8, 10 };
+                   int[] B = new int[] { 1, 3, 5, 7, 9 };
+                   int[] C = new int[] { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+
+
+               Console.WriteLine("Average of A: " + App.getMean(A));
+
+               Console.WriteLine("Average of B: " + App.getMean(B));
+
+                Console.WriteLine("Average of C: " + App.getMean(C));
+
+
+                Console.WriteLine("Reverse the array");
+
+                Console.Write("Reverse of A: "); App.reverse(A);
+
+                Console.Write("Reverse of B: "); App.reverse(B);
+
+                Console.Write("Reverse of C: "); App.reverse(C);
+
+
+                Console.WriteLine("Rotates the array");
+
+                Console.Write("Rotation of A: "); App.rotate("left", 2, A);
+
+                Console.Write("Rotation of B: "); App.rotate("right", 2, B);
+
+                Console.Write("Rotation of C: "); App.rotate("left", 4, C);
+
+
+                Console.WriteLine("Sorts the array");
+
+                Console.Write("Avg of C: "); App.bubbleSort(C);
+
+            }
+
+        }
+
+}
