25b683ccc5f6c466676327ac24066dc24637fa56
Richard Perez <PEREZR27@my.erau.edu>
2020-08-20 15:26:07 -0500

    Adds Roulette to Excercises

---
+++ Roulette/Roulette/Roulette/Bet.cs
@@ -0,0 +1,961 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+
+
+namespace Roulette
+
+{
+
+    class Bet
+
+    {
+
+        public string NumBet(int ball, int option, int[] Numbers)
+
+        {
+
+            string x = "";
+
+            for (int i = 0; i < 1; i++)
+
+            {
+
+                if (option == Numbers[ball])
+
+                {
+
+                    x += $"The ball landed in bin {ball}\n" +
+
+                         $"You win!";
+
+                }
+
+                else if (option != Numbers[ball])
+
+                {
+
+                    x += $"The ball landed in bin {ball}\n" +
+
+                         $"You lose!";
+
+                }
+
+            }
+
+            return x;
+
+        }
+
+        public string evenOdds(int ball, int option, int[] Numbers)
+
+        {
+
+            string x = "";
+
+            for (int i = 0; i < 1; i++)
+
+            {
+
+                if (option == 1)//
+
+                {
+
+                    if (ball % 2 == 0)
+
+                    {
+
+                        x += $"The number is {ball} and it is even.\n" +
+
+                             "You win!";
+
+                    }
+
+                    else if (ball % 2 != 0)
+
+                    {
+
+                        x += $"The number is {ball} and it is odd.\n" +
+
+                            $"You lose";
+
+                    }
+
+
+
+                }
+
+
+
+                if (option == 2)//
+
+                {
+
+                    if (ball % 2 == 0)
+
+                    {
+
+                        x += $"The number is {ball} and it is even.\n" +
+
+                             "You lose";
+
+                    }
+
+                    else if (ball % 2 != 0)
+
+                    {
+
+                        x += $"The number is {ball} and it is odd.\n" +
+
+                            $"You win!";
+
+                    }
+
+
+
+                }
+
+            }
+
+            return x;
+
+        }
+
+        public string RedOrBlack(int ball, int option, string[] Colors)
+
+        {
+
+            string x = "";
+
+            for (int i = 0; i < 1; i++)
+
+            {
+
+                if (option == 1)//
+
+                {
+
+                    if (Colors[ball] == "black")
+
+                    {
+
+                        x += $"The number is {ball} and it is black.\n" +
+
+                             "You win!";
+
+                    }
+
+                    else
+
+                    {
+
+                        x += $"The number is {ball} and it is red.\n" +
+
+                            $"You lose";
+
+                    }
+
+
+
+                }
+
+
+
+                if (option == 2)//
+
+                {
+
+                    if (Colors[ball] == "red")
+
+                    {
+
+                        x += $"The number is {ball} and it is black.\n" +
+
+                             "You lose";
+
+                    }
+
+                    else
+
+                    {
+
+                        x += $"The number is {ball} and it is red.\n" +
+
+                            $"You win!";
+
+                    }
+
+
+
+                }
+
+            }
+
+            return x;
+
+
+
+        }
+
+        public string LowOrHigh(int ball, int option, string[] Number)
+
+        {
+
+            string x = "";
+
+            if (option == 1)
+
+            {
+
+                if (ball < 19 && ball > 0)
+
+                {
+
+
+
+                    x += $"The number is {Number[ball]} and it is a Low.\n" +
+
+                         $"You win!";
+
+                }
+
+                else
+
+                {
+
+                    x += $"Your number is {Number[ball]}. and it is not a Low\n You lose.";
+
+                }
+
+            }
+
+            if (option == 2)
+
+            {
+
+                if (ball > 18 && Number[ball] != Number[37])
+
+                {
+
+                    x += $"The number is {Number[ball]} and it is a High.\n" +
+
+                         $"You win!";
+
+                }
+
+                else
+
+                {
+
+                    x += $"Your number is {Number[ball]}. and it is not a High\n You lose.";
+
+                }
+
+            }
+
+
+
+            return x;
+
+        }
+
+        public string Dozens(int ball, int option, string[] Number)
+
+        {
+
+            string x = "";
+
+            if (option == 1)
+
+            {
+
+                if (ball > 0 && ball < 13)
+
+                {
+
+                    x += $"The number is {Number[ball]}. And it falls within the first dozen.\n" +
+
+                        $"You win!";
+
+                }
+
+                else
+
+                {
+
+                    x += $"The number is {Number[ball]}. And it doesn't falls within the first dozen.\n" +
+
+                        $"You lose.";
+
+
+
+                }
+
+
+
+            }
+
+
+
+            if (option == 2)
+
+            {
+
+                if (ball > 12 && ball < 25)
+
+                {
+
+                    x += $"The number is {Number[ball]}. And it falls within the second dozen.\n" +
+
+                        $"You win!";
+
+                }
+
+                else
+
+                {
+
+                    x += $"The number is {Number[ball]}. And it doesn't falls within the second dozen.\n" +
+
+                        $"You lose.";
+
+
+
+                }
+
+
+
+            }
+
+
+
+            if (option == 3)
+
+            {
+
+                if (ball > 24 && ball < 37)
+
+                {
+
+                    x += $"The number is {Number[ball]}. And it falls within the third dozen.\n" +
+
+                        $"You win!";
+
+                }
+
+                else
+
+                {
+
+                    x += $"The number is {Number[ball]}. And it doesn't falls within the third dozen.\n" +
+
+                        $"You lose.";
+
+
+
+                }
+
+
+
+            }
+
+            return x;
+
+        }
+
+        public string Columns(int ball, int option, string[] Number)
+
+        {
+
+            string x = "";
+
+
+
+            if (option == 1)
+
+            {
+
+                for (int i = 1; i < Number.Length - 1; i += 3)
+
+                {
+
+                    if (ball == int.Parse(Number[i]))
+
+                    {
+
+                        return $"Your ball landed on {Number[ball]} which is on the first column.\n" +
+
+                             "you win!\n";
+
+                    }
+
+                }
+
+
+
+                return $"Your ball landed on {Number[ball]} which is not on the first column.\n" +
+
+                     "you lose\n";
+
+            }
+
+            if (option == 2)
+
+            {
+
+                for (int i = 2; i < Number.Length - 1; i += 3)
+
+                {
+
+                    if (ball == int.Parse(Number[i]))
+
+                    {
+
+                        return $"Your ball landed on {Number[ball]} which is on the second column.\n" +
+
+                             "you win!\n";
+
+                    }
+
+                }
+
+
+
+                return $"Your ball landed on {Number[ball]} which is not on the second column.\n" +
+
+                     "you lose\n";
+
+            }
+
+            if (option == 3)
+
+            {
+
+                for (int i = 3; i < Number.Length - 1; i += 3)
+
+                {
+
+                    if (ball == int.Parse(Number[i]))
+
+                    {
+
+                        return $"Your ball landed on {Number[ball]} which is on the third column.\n" +
+
+                             "you win!\n";
+
+                    }
+
+                }
+
+
+
+                return $"Your ball landed on {Number[ball]} which is not on the third column.\n" +
+
+                     "you lose\n";
+
+            }
+
+
+
+            return x;
+
+        }
+
+        public string Streets(int ball, int option, string[] Number)
+
+        {
+
+
+
+            switch (option)
+
+            {
+
+                case (1):
+
+                    for (int i = 1; i < 4; i++)
+
+                    {
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+
+
+                    }
+
+                    break;
+
+
+
+                case (2):
+
+                    for (int i = 4; i < 7; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (3):
+
+                    for (int i = 7; i < 10; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (4):
+
+                    for (int i = 10; i < 13; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (5):
+
+                    for (int i = 13; i < 16; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (6):
+
+                    for (int i = 16; i < 19; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (7):
+
+                    for (int i = 19; i < 21; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (8):
+
+                    for (int i = 21; i < 24; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (9):
+
+                    for (int i = 24; i < 27; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (10):
+
+                    for (int i = 27; i < 30; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (11):
+
+                    for (int i = 30; i < 33; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (12):
+
+                    for (int i = 33; i <= 36; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+            }
+
+            return $"Your ball landed on {ball}\n You lose\n";
+
+        }
+
+        public string SixNumbers(int ball, int option, string[] Number)
+
+        {
+
+            switch (option)
+
+            {
+
+                case (1):
+
+                    for (int i = 1; i < 7; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+
+
+                    }
+
+                    break;
+
+
+
+                case (2):
+
+                    for (int i = 4; i < 10; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (3):
+
+                    for (int i = 7; i < 13; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (4):
+
+                    for (int i = 10; i < 16; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (5):
+
+                    for (int i = 13; i < 19; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (6):
+
+                    for (int i = 16; i < 22; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (7):
+
+                    for (int i = 19; i < 25; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (8):
+
+                    for (int i = 21; i < 28; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (9):
+
+                    for (int i = 24; i < 31; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (10):
+
+                    for (int i = 27; i < 34; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+
+
+                case (11):
+
+                    for (int i = 30; i < 37; i++)
+
+                    {
+
+                        if (ball == int.Parse(Number[i]))
+
+                        {
+
+                            return $"your ball landed on {ball} you win!";
+
+                        }
+
+                    }
+
+                    break;
+
+            }
+
+            return $"Your ball landed on {ball}\n You lose\n";
+
+        }
+
+
+    }
+
+}
---
+++ Roulette/Roulette/Roulette/Program.cs
@@ -0,0 +1,313 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace Roulette
+
+{
+
+    class Program
+
+    {
+
+        static void Main()
+
+        {
+
+            Bet bets = new Bet();
+
+            int[] Numbers = new int[38];
+
+            string[] Colors = new string[38] {"green", "red", "black", "red", "black", "red", "black", "red", "black", "red", "black", "black", "red", "black",
+
+            "red", "black", "red", "black", "red", "red", "black", "red", "black", "red", "black", "red", "black","red", "black", "black", "red", "black", "red","black",
+
+            "red", "black", "red", "green"};
+
+            string[] Number = new string[38] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
+
+            "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "00"};
+
+            string[,] Table = new string[3, 12]
+
+            {
+
+                { "3","6","9", "12", "15", "18", "21", "24" , "27", "30", "33", "36"},
+
+                { "2", "5", "8", "11", "14", "17", "20", "23", "26", "29", "32", "35"},
+
+                { "1","4", "7", "10", "13", "16", "19", "22", "25", "28", "31", "34"}
+
+            };
+
+            Random rand = new Random();
+
+
+
+            int ball = rand.Next(0, 38);
+
+
+
+            Console.WriteLine($"Place your bet:\n" +
+
+                $"[1] Numbers\n" +
+
+                $"[2] Even/Odds\n" +
+
+                $"[3] Reds/Blacks\n" +
+
+                $"[4] Lows/Highs\n" +
+
+                $"[5] Dozens\n" +
+
+                $"[6] Columns\n" +
+
+                $"[7] Street\n" +
+
+                $"[8] 6 Numbers\n" +
+
+                $"[9] Split\n" +
+
+                $"[10] Corner\n");
+
+
+
+            int choice = int.Parse(Console.ReadLine());
+
+
+
+            switch (choice)
+
+            {
+
+                case (1):
+
+                    {
+
+                        Console.WriteLine("Pick a number: \n 1 - 36");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.NumBet(ball, option, Numbers));
+
+                        break;
+
+                    }
+
+                case (2):
+
+                    {
+
+                        Console.WriteLine("even or odds?\n" +
+
+                                          "[1] even\n" +
+
+                                          "[2] odd\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.evenOdds(ball, option, Numbers));
+
+                        break;
+
+                    }
+
+                case (3):
+
+                    {
+
+                        Console.WriteLine("black or red?\n" +
+
+                            "[1] black\n" +
+
+                            "[2] red\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.RedOrBlack(ball, option, Colors));
+
+                        break;
+
+                    }
+
+                case (4):
+
+                    {
+
+                        Console.WriteLine("Lows or Highs?\n" +
+
+                            "[1] Lows\n" +
+
+                            "[2] Highs\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.LowOrHigh(ball, option, Number));
+
+                        break;
+
+                    }
+
+                case (5):
+
+                    {
+
+                        Console.WriteLine("Pick a dozen\n" +
+
+                            "[1] 1-12\n" +
+
+                            "[2] 13-34\n" +
+
+                            "[3] 25-36\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.Dozens(ball, option, Number));
+
+                        break;
+
+                    }
+
+                case (6):
+
+                    {
+
+                        Console.WriteLine("Pick a column\n" +
+
+                            "[1] Contains (1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34\n" +
+
+                            "[2] Contains (2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35\n" +
+
+                            "[3] Contains (3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.Columns(ball, option, Number));
+
+                        break;
+
+                    }
+
+                case (7):
+
+                    {
+
+                        Console.WriteLine("Pick a row\n" +
+
+                            "[1] 1,2,3\n" +
+
+                            "[2] 4,5,6\n" +
+
+                            "[3] 7,8,9\n" +
+
+                            "[4] 10,11,12\n" +
+
+                            "[5] 13,14,15\n" +
+
+                            "[6] 16,17,18\n" +
+
+                            "[7] 19,20,21\n" +
+
+                            "[8] 22,23,24\n" +
+
+                            "[9] 25,26,27\n" +
+
+                            "[10] 28,29,30\n" +
+
+                            "[11] 31,32,33\n" +
+
+                            "[12] 34,35,36\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.Streets(ball, option, Number));
+
+                        break;
+
+                    }
+
+                case (8):
+
+                    {
+
+                        Console.WriteLine("Pick a double row\n\n" +
+
+                            "[1] 1,2,3 and 4,5,6\n" +
+
+                            "[2] 4,5,6 and 7,8,9\n" +
+
+                            "[3] 7,8,9 and 10,11,12\n" +
+
+                            "[4] 10,11,12 and 13,14,15\n" +
+
+                            "[5] 13,14,15 and 16,17,18\n" +
+
+                            "[6] 16,17,18 and 19,20,21\n" +
+
+                            "[7] 19,20,21 and 22,23,24\n" +
+
+                            "[8] 22,23,24 and 25,26,27\n" +
+
+                            "[9] 25,26,27 and 28,29,30\n" +
+
+                            "[10] 28,29,30 and 31,32,33\n" +
+
+                            "[11] 31,32,33 and 34,35,36\n");
+
+                        int option = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine(bets.SixNumbers(ball, option, Number));
+
+                        break;
+
+                    }
+
+                case (9):
+
+                    {
+
+                        Console.WriteLine("Pick two contiguous numbers\n" +
+
+                             "3", "6", "9", "12", "15", "18", "21", "24", "27", "30", "33", "36\n" +
+
+                             "2", "5", "8", "11", "14", "17", "20", "23", "26", "29", "32", "35\n" +
+
+                             "1", "4", "7", "10", "13", "16", "19", "22", "25", "28", "31", "34\n" +
+
+                             "Enter your first number\n");
+
+                        int opt1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter your second number");
+
+                        int opt2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("....");
+
+
+
+
+
+                        break;
+
+                    }
+
+                case (10):
+
+                    {
+
+                        Console.WriteLine("This bet is still under construction");
+
+                        break;
+
+                    }
+
+            }
+
+        }
+
+    }
+
+}
